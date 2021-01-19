8776b7fcbaf90ed1f1414d275d3c319b5a885d0e
phd2033 <hdpark@boryung.co.kr>
2021-01-19 15:01:16 +0900

    tt33

--- SUI/boryung/보령/WMS/빈용기무게측정ViewModel.cs
+++ SUI/boryung/보령/WMS/빈용기무게측정ViewModel.cs
@@ -128,6 +128,8 @@ namespace 보령
                 {
                     try
                     {
+                        //test
+                        //1111
                         CommandCanExecutes["LoadedCommandAsync"] = false;
                         CommandResults["LoadedCommandAsync"] = false;
 
