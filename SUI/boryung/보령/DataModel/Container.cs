0f865f3c48e6b3d8330cba5be66af4daee26a5a5
LGCNS <brmes@boryung.co.kr>
2021-05-20 19:29:29 +0900

    [SUI-Custom-보령] 포장완제품입고3 지관공정중제품을 스캔하여 입고수량을 자동계산한다. 투입버튼 클릭 시 마지막 팔레트인 경우 미입고 LOT이 있는지 확인한다. 입고대기 Lot은 기록 시 투입처리 되고 전송한 입고량으로 OUTPUT이 생성된다. 완제품입고 비즈룰에서 LOT이 전부 투입된 경우 LASTPALLET 컬럼을 Y로 전송한다.

---
+++ SUI/boryung/보령/DataModel/Container.cs
@@ -0,0 +1,93 @@
+﻿using LGCNS.iPharmMES.Common;
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace 보령
+{
+    public abstract class Container : BizActorDataSetBase
+    {
+        private string _MsubLotId;
+        public string MsubLotId
+        {
+            get { return _MsubLotId; }
+            set
+            {
+                _MsubLotId = value;
+                OnPropertyChanged("MsubLotId");
+            }
+        }
+        private string _Barcode;
+        public string Barcode
+        {
+            get { return _Barcode; }
+            set
+            {
+                _Barcode = value;
+                OnPropertyChanged("Barcode");
+            }
+        }
+        private string _VesselId;
+        public string VesselId
+        {
+            get { return _VesselId; }
+            set
+            {
+                _VesselId = value;
+                OnPropertyChanged("VesselId");
+            }
+        }
+        private int _Precision;
+        public int Precision
+        {
+            get { return _Precision; }
+            set
+            {
+                _Precision = value;
+                OnPropertyChanged("Precision");
+            }
+        }
+        private string _Uom;
+        public string Uom
+        {
+            get { return _Uom; }
+            set
+            {
+                _Uom = value;
+                OnPropertyChanged("Uom");
+            }
+        }
+        private decimal _NetWeight;
+        public decimal NetWeight
+        {
+            get { return _NetWeight; }
+            set
+            {
+                _NetWeight = value;
+                OnPropertyChanged("NetWeight");
+            }
+        }
+        private decimal _TareWeight;
+        public decimal TareWeight
+        {
+            get { return _TareWeight; }
+            set
+            {
+                _TareWeight = value;
+                OnPropertyChanged("TareWeight");
+            }
+        }
+        public decimal GrossWeight
+        {
+            get { return _NetWeight + _TareWeight; }
+        }
+
+    }
+}
